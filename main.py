91244e4fe526ef8f7b2af0d93dd6e8f9fa71bef1
takigavin <gavinhorner0@gmail.com>
2020-11-21 18:03:53 -0600

    Option 1 close, and option 5 completed

--- main.py
+++ main.py
@@ -9,17 +9,30 @@
 import pandas as pd
 
 flights_df = pd.read_csv("Data_Train.csv")  # Open our data file and store it as a data frame for use within the methods
-print(flights_df)
 unique_airlines = set(flights_df.Airline)  # A set of unique airlines declared early for use in option 9.
-unique_airlines_list = list(
-    unique_airlines)  # A list of unique airlines for easy index access declared early for use in option 7
+unique_airlines_list = list(unique_airlines)  # A list of unique airlines for easy index access declared early for use in option 7
+num_of_flights = len(flights_df.Airline) # Length of the dataset for use in any option 1 and 7
 
+pd.set_option('display.width', 320)
+pd.set_option("display.max_columns", 11)  # All necessary for proper formatting in the Run output box
+pd.set_option("display.max_rows", 10683)  # Comment out as needed
 
-# Option 1 (Gavin)
+# Option 1 (Gavin) IN PROGRESS
 def search_by_source_destination():
     print("You chose option 1.")
-    day = input("Please enter a day:")
-    month = input("Please enter a month:")
+    source = input("Please enter a source: ")
+    destination = input("Please enter a destination: ")
+    day = input("Please enter a day: ")
+    month = input("Please enter a month: ")
+
+    rows = []
+    for i in range(0, num_of_flights):
+        flight_day, flight_month, flight_year = flights_df.Date_of_Journey[i].split('/')
+        if flights_df.Source[i] == source:
+            if flights_df.Destination[i] == destination:
+                if flight_day == day:
+                    if flight_month == month:
+                        rows.append([flights_df.iloc[[i]]])
 
 
 # Option 2 (Evan)
@@ -37,9 +50,13 @@ def find_min_dur():
     print("You chose option 4.")
 
 
-# Option 5 (Gavin)
+# Option 5 (Gavin) COMPLETED
 def search_specific_route():
     print("You chose option 5.")
+    print('____________________________________')
+    user_route = input('Enter your specific route (ex: BLR to BOM to AMD to DEL): ')
+    full_route = user_route.replace('to', '?')  # Because the dataset is formatted with ? between each stop for some reason
+    print(flights_df[flights_df.Route == full_route]) # Prints any flight with the matching route from the dataset
 
 
 # Option 6 (Evan)
@@ -52,21 +69,20 @@ def display_summary_of_airline():
     print("You chose option 7.")
     print('____________________________________')
     num_of_unique_airlines = len(unique_airlines)
-    num_of_flights = len(flights_df.Airline)
     for i in range(0, num_of_unique_airlines):  # For every unique airline
         max = 0  # Set max to 0 for initialization
         min = 1000000000  # Set min to arbitrary number for initialization
         for j in range(0, num_of_flights):  # For every flight
             if unique_airlines_list[i] == flights_df.Airline[j]:  # If the unique airline and the current flight's
-                if flights_df.Price[j] > max:                     # airline matches, and if the price of that flight is
-                    max = flights_df.Price[j]                     # greater than the current max, then set the max to
-                                                                  # that price.
-                if flights_df.Price[j] < min:                     # If the price is lower than the min, then set the min
-                    min = flights_df.Price[j]                     # to that price.
+                if flights_df.Price[j] > max:  # airline matches, and if the price of that flight is
+                    max = flights_df.Price[j]  # greater than the current max, then set the max to
+                    # that price.
+                if flights_df.Price[j] < min:  # If the price is lower than the min, then set the min
+                    min = flights_df.Price[j]  # to that price.
 
         print(f'{unique_airlines_list[i]} max: {max}')  # Print our airline and its respective max price
         print(f'{unique_airlines_list[i]} min: {min}')  # Print our airline and its respective min price
-        if i != num_of_unique_airlines - 1: # If statement for formatting to not print a dotted line at the very end
+        if i != num_of_unique_airlines - 1:  # If statement for formatting to not print a dotted line at the very end
             print('------------------------------------')
     print('____________________________________')
 
